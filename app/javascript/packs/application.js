7d64aa04b3197f9617ff2078c82798fa95b9004f
yuyaosako <yuyaosako070864@gmail.com>
2024-02-01 12:00:02 +0900

    レイアウト周りのベース

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require turbolinks
+//= require_tree .
+
+// Bootstrap4
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets  
